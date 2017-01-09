fef85a8974933309cb4bede0ad1fbdff3a779a56
Leandro <lrodriguez@sparkdigital.com>
2017-01-09 15:25:51 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/list', function(req, res, next) {
+  res.render('book-list', { title: 'Express list' });
+});
+
+module.exports = router;
