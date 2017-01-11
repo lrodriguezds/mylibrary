7c987db7fde6e04d16259ec7cf6a0ce5d38677d3
Leandro <lrodriguez@sparkdigital.com>
2017-01-11 17:13:39 -0300

    fdsafs

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+//HOME PAGE
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//LIST
 router.get('/list', function(req, res, next) {
   res.render('book-list', { title: 'Express list', 'btnListActive': true });
 });
 
+//ADD BOOK
 router.get('/add', function(req, res, next) {
   res.render('add-book', { title: 'Express list', 'btnAddActive': true });
 });
+router.post('/add', function(req, res, next) {
+  
+  console.log(req.body);
 
+  res.render('add-book', { title: 'Express list', 'btnAddActive': true });
+});
 
+//ABOUT
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Express list', 'btnAboutActive': true });
 });
