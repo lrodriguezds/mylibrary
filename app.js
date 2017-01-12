746c8637153d5ac74dced5a05f2d98e0d09e6dde
Leandro <lrodriguez@sparkdigital.com>
2017-01-12 16:27:31 -0300

    fdsfs

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var users = require('./routes/users');
 
 var app = express();
 
+/*var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/test");
+
+var bookSchema = {
+	title: String,
+	description: String
+}
+var book = mongoose.model("Book", bookSchema);*/
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/test");
+
+var bookSchema = {
+	title: String,
+	description: String
+}
+var Book = mongoose.model("Book", bookSchema);
 
 //HOME PAGE
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 //LIST
 router.get('/list', function(req, res, next) {
-  res.render('book-list', { title: 'Express list', 'btnListActive': true });
+
+	Book.find(function(err, books) {
+		console.log(books);
+		
+		res.render('book-list', { title: 'Express list', 'btnListActive': true, books: books });
+	});
+	
+	
 });
 
 //ADD BOOK
@@ -17,9 +33,19 @@ router.get('/add', function(req, res, next) {
 });
 router.post('/add', function(req, res, next) {
   
-  console.log(req.body);
+	console.log(req.body);
 
-  res.render('add-book', { title: 'Express list', 'btnAddActive': true });
+	var data = {
+		title: req.body.title,
+		description: req.body.description
+	}
+
+	var book = new Book(data);
+	book.save(function(err) {
+		console.log(book);
+	});
+
+	res.render('add-book', { title: 'Express list', 'btnAddActive': true });
 });
 
 //ABOUT
